ea86145c3b9353084b09adbe4e618aa3e23f952f
Camden Blatchly <camden.blatchly@ruralinnovation.us>
2025-01-08 15:11:38 -0500

    working hover effect and tooltip

--- src/components/CountyChoropleth.tsx
+++ src/components/CountyChoropleth.tsx
@@ -1,125 +1,126 @@
-import React, { useState, useCallback, useEffect, useRef } from 'react';
-import Map, { Source, Layer, MapRef } from 'react-map-gl';
-import type { FillLayer } from 'react-map-gl';
-import { fitBounds } from '@math.gl/web-mercator';
-import { format } from 'd3-format';
+import React, { useState, useMemo } from 'react';
+import DeckGL from '@deck.gl/react';
+import { GeoJsonLayer } from '@deck.gl/layers';
+import { Map } from 'react-map-gl';
+import { WebMercatorViewport } from '@deck.gl/core';
 import { Feature } from 'geojson';
-
-// import { mean, median } from 'd3-array';
+import { format } from 'd3-format';
 import { jenks } from 'simple-statistics';
 
 // Constants
 const USA_BOUNDS: [[number, number], [number, number]] = [
-  [-125, 24], // Southwest coordinates: [Longitude, Latitude]
-  [-66, 49],  // Northeast coordinates: [Longitude, Latitude]
+  [-125, 24], // Southwest coordinates
+  [-66, 49],  // Northeast coordinates
 ];
 
 const percentFormat = format('.1%');
 const dollarFormat = format('$,.0f');
 const MAPBOX_TOKEN = import.meta.env.VITE_MAPBOX_TOKEN;
 
+const INITIAL_VIEW_STATE = {
+  longitude: -95.5,
+  latitude: 37,
+  zoom: 3.8,
+  pitch: 0,
+  bearing: 0
+};
+
 // Props type
 interface CountyChoroplethProps {
   geojsonData: GeoJSON.FeatureCollection;
 }
 
-// HoverInfo type
-interface HoverInfo {
-  feature: Feature;
-  x: number;
-  y: number;
-}
-
 // Component
 const CountyChoropleth: React.FC<CountyChoroplethProps> = ({ geojsonData }) => {
-  const mapRef = useRef<MapRef>(null);
+  const [hoverInfo, setHoverInfo] = useState<{
+    object: Feature;
+    x: number;
+    y: number;
+  } | null>(null);
 
-  const { longitude, latitude, zoom } = fitBounds({
-    width: window.innerWidth > 1000 ? 1000 : window.innerWidth,
-    height: 500,
-    bounds: USA_BOUNDS,
-    padding: 20, // Optional padding around the bounds
-  });
+  // Memoize color calculations
+  const { breaks, getColorForValue } = useMemo(() => {
+    const scale_dta = geojsonData.features.map(
+      (feature) => feature.properties?.amount_raised_per_capita
+    );
+    const numClasses = 5;
+    const breaks = jenks(scale_dta, numClasses);
+    const colors = ['#FAF7E8', '#A3D9C5', '#7DBAA3', '#46827E', '#245A61', '#16343E'];
 
-  const scale_dta = geojsonData.features.map((feature) => feature.properties?.amount_raised_per_capita);
-
-  const numClasses = 5;
-
-  // Calculate Jenks breaks
-  const breaks = jenks(scale_dta, numClasses);
-  const colors = ['#FAF7E8', '#A3D9C5', '#7DBAA3', '#46827E', '#245A61', '#16343E'];
-  const stops = breaks.map((breakpoint, i) => [breakpoint, colors[i]]);
-
-  const dataLayer: FillLayer = {
-    id: 'data',
-    type: 'fill',
-    paint: {
-      'fill-color': {
-        property: 'amount_raised_per_capita',
-        stops: stops,
-      },
-      'fill-opacity': 0.8,
-      'fill-outline-color': '#000000'
-    }
-  };
-  
-
-  const [hoverInfo, setHoverInfo] = useState<HoverInfo | null>(null);
-
-  const onHover = useCallback((event: mapboxgl.MapMouseEvent & mapboxgl.EventData) => {
-    const {
-      features,
-      point: { x, y },
-    } = event;
-    const hoveredFeature = features && features[0];
-
-    // Update hover info if there's a valid feature
-    setHoverInfo(hoveredFeature ? { feature: hoveredFeature, x, y } : null);
-  }, []);
-
-  useEffect(() => {
-    const resizeMap = () => {
-      if (mapRef.current) {
-        const map = mapRef.current.getMap();
-        map.fitBounds(USA_BOUNDS, { padding: 20 });
+    const getColorForValue = (value: number) => {
+      for (let i = 0; i < breaks.length; i++) {
+        if (value <= breaks[i]) {
+          return colors[i];
+        }
       }
+      return colors[colors.length - 1];
     };
 
-    window.addEventListener('resize', resizeMap);
-    resizeMap(); // Call resizeMap on component mount to fit bounds on load
+    return { breaks, getColorForValue };
+  }, [geojsonData]);
 
-    return () => {
-      window.removeEventListener('resize', resizeMap);
-    };
-  }, []);
+  const layers = useMemo(() => [
+    new GeoJsonLayer({
+      id: 'counties',
+      data: geojsonData,
+      pickable: true,
+      stroked: true,
+      filled: true,
+      getFillColor: (d: any) => {
+        const color = getColorForValue(d.properties.amount_raised_per_capita);
+        const r = parseInt(color.slice(1, 3), 16);
+        const g = parseInt(color.slice(3, 5), 16);
+        const b = parseInt(color.slice(5, 7), 16);
+        return [r, g, b, 200];
+      },
+      autoHighlight: true,
+      highlightColor: [255, 165, 0, 255],
+      getLineColor: [0, 0, 0, 255],
+      lineWidthMinPixels: .25,
+      updateTriggers: {
+        getFillColor: [breaks]
+      }
+    })
+  ], [geojsonData, breaks, getColorForValue]);
+
+  const onHover = (info: any) => {
+    setHoverInfo(info.object ? {
+      object: info.object,
+      x: info.x,
+      y: info.y,
+    } : null);
+  };
 
   return (
-    <Map
-      ref={mapRef}
-      initialViewState={{
-        latitude,
-        longitude,
-        zoom,
-      }}
-      mapStyle="mapbox://styles/mapbox/light-v9"
-      mapboxAccessToken={MAPBOX_TOKEN}
-      interactiveLayerIds={['data']}
-      onMouseMove={onHover}
-    >
-      <Source type="geojson" data={geojsonData}>
-        <Layer {...dataLayer} />
-      </Source>
-      {hoverInfo && (
-        <div className="tooltip" style={{ left: hoverInfo.x, top: hoverInfo.y }}>
-          <div>
-            <p>
-              <b>{hoverInfo.feature.properties?.name_co}</b><br></br>
-              Amount raised per capita: {dollarFormat(hoverInfo.feature.properties?.amount_raised_per_capita)}
-            </p>
-          </div>
-        </div>
-      )}
-    </Map>
+    <div style={{ position: 'relative', height: '100vh', width: '100vw' }}>
+      <DeckGL
+        initialViewState={INITIAL_VIEW_STATE}
+        controller={true}
+        layers={layers}
+        onHover={onHover}
+        getTooltip={({object}) => object && {
+          html: `
+            <div style="padding: 8px">
+              <b>${object.properties?.name_co}</b><br/>
+              Amount raised per capita: ${dollarFormat(object.properties?.amount_raised_per_capita)}
+            </div>
+          `,
+          style: {
+            backgroundColor: 'white',
+            fontSize: '12px',
+            borderRadius: '4px',
+            color: 'black',
+            border: '1px solid black'
+          }
+        }}
+      >
+        <Map
+          mapStyle="mapbox://styles/mapbox/light-v9"
+          mapboxAccessToken={MAPBOX_TOKEN}
+          reuseMaps
+        />
+      </DeckGL>
+    </div>
   );
 };
 
