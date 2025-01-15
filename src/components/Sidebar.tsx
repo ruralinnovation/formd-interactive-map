057abac5ea734c2fc7f601b2a434ae2598e60f42
Camden Blatchly <camden.blatchly@ruralinnovation.us>
2025-01-15 13:48:36 -0500

    added in N count

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -33,37 +33,40 @@ const Sidebar: React.FC<SidebarProps> = ({ selected_county, data }) => {
         <hr />
       </div>
       {selected_county ? (
-        <h2>{selected_county.name}</h2>
+        <>
+          <h2>{selected_county.name}</h2>
+          <p>
+            Businesses raising private capital: {sortedData?.length.toLocaleString('en-US')}
+          </p>
+        </>
       ) : (
         <p>Click a county to view more detailed information</p>
       )}
 
       <div className={style['county-detail-wrapper']}>
-      {sortedData && sortedData.length > 0 ? (
-        sortedData.map((county, index) => (
-          <div key={index} className={style['county-detail']}>
-            <h3>{county.entity_name}</h3>
-            <table className={style['county-table']}>
-              <tbody>
-                <tr>
-                  <td>Total Amount Raised</td>
-                  <td>{county.total_amount_raised.toLocaleString('en-US', { style: 'currency', currency: 'USD' })}</td>
-                </tr>
-                <tr>
-                  <td>Filing year(s)</td>
-                  <td>{county.filing_years}</td>
-                </tr>
-                <tr>
-                  <td>Industry</td>
-                  <td>{county.industry}</td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
-        ))
-      ) : selected_county ? (
-        <p>No data available for the selected county</p>
-      ) : null}
+        {sortedData && sortedData.length > 0 ? (
+          sortedData.map((county, index) => (
+            <div key={index} className={style['county-detail']}>
+              <h3>{county.entity_name}</h3>
+              <table className={style['county-table']}>
+                <tbody>
+                  <tr>
+                    <td>Total Amount Raised</td>
+                    <td>{county.total_amount_raised.toLocaleString('en-US', { style: 'currency', currency: 'USD' })}</td>
+                  </tr>
+                  <tr>
+                    <td>Filing year(s)</td>
+                    <td>{county.filing_years}</td>
+                  </tr>
+                  <tr>
+                    <td>Industry</td>
+                    <td>{county.industry}</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          ))
+        ) : null}
       </div>
     </div>
   );
